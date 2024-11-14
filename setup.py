0b55264318514eb0f75bf35ecd9b7915c50760a8
Kerim Can Karaduman <96576479+KcanCurly@users.noreply.github.com>
2024-11-14 21:53:31 +0300

    setup.py 8

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     python_requires=">=3.8",
     entry_points={
         "console_scripts": [
-            "FindUncommonShares=finduncommonshares.finduncommonshares:main",  
+            "FindUncommonShares=FindUncommonShares.finduncommonshares:main",  
         ],
     },
 )
